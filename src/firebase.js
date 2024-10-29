9d84b884b6180285df97278890d97ce3cb4a7121
Mico Suarez <162002831+micotin@users.noreply.github.com>
2024-10-29 19:46:10 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDFxlUNxq-MxjnLKYTBSDb2n0i8gYSV0gg",
-  authDomain: "sam1-9f4ed.firebaseapp.com",
-  projectId: "sam1-9f4ed",
-  storageBucket: "sam1-9f4ed.appspot.com",
-  messagingSenderId: "698554699726",
-  appId: "1:698554699726:web:232f94ea216f5d1e0ea59f",
-  measurementId: "G-5DR3NCMK22"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "
+  measurementId: ""
 };
 
 // Initialize Firebase
